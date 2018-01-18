b9818623aad3004e43646fde3a030636a0bdd296
胡耀汉 <huyh@mysoft.com.cn>
2018-01-18 11:16:46 +0800

    demo 重新更改 支持es6 rest-spread

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ var compiler = webpack(config);
 
 app.use(require("webpack-dev-middleware")(compiler, {
     noInfo: true, 
-    publicPath: config.output.publicPath
+    publicPath: config.output.publicPath,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000 // 每秒轮询检查文件变动
+    }
 }));
 
 app.use(require("webpack-hot-middleware")(compiler, {
--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, browserHistory } from 'react-router'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-import withExampleBasename from './withExampleBasename'
-import './stubs/COURSES'
-
-const rootRoute = {
-  childRoutes: [ {
-    path: '/',
-    component: require('./components/App'),
-    childRoutes: [
-      require('./routes/Calendar'),
-      require('./routes/Course'),
-      require('./routes/Grades')
-    ]
-  } ]
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>app1</h1>
+                <div>content</div>
+                <div>AAHHAAfsdssss</div>
+            </div>
+        );
+    }
 }
 
-render((
-  <Router
-    history={browserHistory}
-    routes={rootRoute}
-  />
-), document.getElementById('container'))
+ReactDOM.render(
+    <App />,
+    document.getElementById('container')
+);
